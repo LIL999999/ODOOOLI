26151d44fe60c570277e6c7b72b0fd8350ac97c4
LIL999999 <45680643+LIL999999@users.noreply.github.com>
2018-12-08 03:22:17 +0000

    aa

--- pscloud_training/__manifest__.py
+++ pscloud_training/__manifest__.py
@@ -7,7 +7,9 @@
     'category': 'Tools',
     'depends': ['base'],
     'data': [
-        
+         'security/ir.model.access.csv',
+         'views/training_subject_views.xml',
+         'views/training_views.xml',
     ],
     'demo': [
         
