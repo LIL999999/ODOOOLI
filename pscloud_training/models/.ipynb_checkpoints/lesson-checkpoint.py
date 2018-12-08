f644a2b3c5454b369f5f6a91e28e1c7a7e6fbfcd
LIL999999 <45680643+LIL999999@users.noreply.github.com>
2018-12-08 07:51:21 +0000

    zz

--- pscloud_training/models/.ipynb_checkpoints/lesson-checkpoint.py
+++ pscloud_training/models/.ipynb_checkpoints/lesson-checkpoint.py
@@ -6,6 +6,27 @@ import datetime
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
 
 
+class TrainingLesson(models.Model):
+    _name = 'pscloud.training.lesson'
+    _description = "课程信息"
+
+    @api.multi
+    @api.depends('start_date', 'end_date')
+    def _compute_days(self):
+        for lesson in self:
+            if lesson.start_date and lesson.end_date:
+                start_date = datetime.datetime.strptime(lesson.start_date, DATE_FORMAT) if type('') == type(lesson.start_date) else lesson.start_date
+                end_date = datetime.datetime.strptime(lesson.end_date, DATE_FORMAT) if type('') == type(lesson.end_date) else lesson.end_date
+                lesson.continue_days = (end_date - start_date).days
+
+    state = fields.Selection([
+        ('draft', '草稿'),
+        ('confirm', '确认'),
+        ], string='状态', readonly=True, copy=False, index=True, default='draft')
+    seat_qty = fields.Integer(string='座位数')
+    subject_id = fields.Many2one('pscloud.training.subject', string='科目')
+    person_id = fields.Many2one('res.partner', related='subject_id.person_id', readonly=True)
+    desc = fields.Text(string='描述')
 
 
 
