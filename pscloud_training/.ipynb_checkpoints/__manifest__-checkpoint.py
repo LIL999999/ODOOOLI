c34e1c54763166bd68b0749c0c34d76563fd9602
LIL999999 <45680643+LIL999999@users.noreply.github.com>
2018-12-08 02:41:19 +0000

    as

--- pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,17 +1,21 @@
-# -*- coding: utf-8 -*-
 {
-    'name': 'pscloud_training',
+    'name': 'PScloudx',
     'version': '12.0.1.0',
-    'summary': 'pscloud_training',
+    'summary': 'PScloud_training',
     'author': "www.mypscloud.com",
     'website': 'https://www.mypscloud.com/',
     'category': 'Tools',
     'depends': ['base'],
     'data': [
-    
+        'security/ir.model.access.csv',
+        'wizard/student_regiester_views.xml',
+        'views/res_partner_views.xml',
+        'views/training_lesson_views.xml',
+        'views/training_subject_views.xml',
+        'views/training_views.xml',
     ],
     'demo': [
-
+        'demo/pscloud_demo.xml',
     ],
     'qweb': [],
     'js': [],
